014b96a6fc48ee628f3ba6da9c2f657af498560d
Jens W. Langenberg <william@diversiam.com>
2026-05-03 20:58:46 +0200

    Added build script and adjusted .gitignore

---
+++ build.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+from scripts.paths import *
+from scripts.build_runner import BuildRunner
+from scripts.argument_parser import create_argument_parser
+
+
+def main():
+    parser = create_argument_parser()
+    args = parser.parse_args()
+
+    if not len(sys.argv) > 1:
+        parser.print_help()
+        exit(1)
+
+    ''' Setting up environment '''
+    os.chdir(ROOT_DIR)
+
+    build_dir = get_build_dir(str(args.architecture))
+    test_dir = build_dir / "tests"
+    runner = BuildRunner(root_dir=ROOT_DIR, build_dir=build_dir, project_name="Chess Engine")
+
+    runner.update_environment()
+    runner.update_app_version()
+
+    print("==== Chess Engine Configuration ====")
+    print(f"Current Directory:          {ROOT_DIR}")
+    print(f"Build Directory:            {build_dir}")
+    print(f"Test Build Directory:       {test_dir}")
+    print(f"Architecture:               {args.architecture}")
+    print(f"Configuration:              {args.configuration}")
+    print(f"Environment:                {runner.env}")
+    print(f"Version:                    {runner.version}")
+    print("=====================================")
+
+    if(args.runtest):
+        runner.run_cpp_unit_tests(configuration=args.configuration, test_build_dir=test_dir, target="RUN_TESTS")
+        exit(0)
+
+    runner.create_build_generator(
+        platform=args.platform,
+        build=args.build,
+        architecture=args.architecture,
+        configuration=args.configuration,
+    )
+
+    if not args.prepare:
+        runner.run_cpp_unit_tests(configuration=args.configuration, test_build_dir=test_dir, target="RUN_TESTS")
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/enums.py
@@ -0,0 +1,40 @@
+from enum import Enum
+
+
+class Architecture(Enum):
+    x64     = "x64"
+    ARM64   = "ARM64"
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class Environment(Enum):
+    Development     = "ENV_DEVELOPMENT"
+    Staging         = "ENV_STAGING"
+    Production      = "ENV_PRODUCTION"
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class Configuration(Enum):
+    Debug           = "Debug"
+    Release         = "Release"
+    RelWithDebInfo  = "RelWithDebInfo"
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class Platform(Enum):
+    Ninja       = 'Ninja'
+    VS2022      = 'VS2022'
+    VS2026      = 'VS2026'
+    
+    def __str__(self) -> str:
+        if self is Platform.VS2022:
+            return 'Visual Studio 17 2022'
+        elif self is Platform.VS2026:
+            return 'Visual Studio 18 2026'
+        return self.value
