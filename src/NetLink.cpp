7837259baba40d54cf73a77825cebd5c5aab8ec9
Jens W. Langenberg <william@diversiam.com>
2026-05-08 21:31:26 +0200

    Adjusted pImpl

--- src/NetLink.cpp
+++ src/NetLink.cpp
@@ -12,6 +12,7 @@
 #include "Signaling/RoleNegotiation.h"
 #include "TCP/TCPSession.h"
 #include "Messaging/RemoteCommunication.h"
+#include "ConnectionFlow/ConnectionFlow.h"
 
 
 struct netlink::NetLink::Impl
@@ -22,10 +23,8 @@ struct netlink::NetLink::Impl
 	asio::io_context	ioContext;
 	DiscoveryService	discovery{ioContext};
 	SignalingService	signaling{ioContext};
+	ConnectionFlow		connectionFlow{ioContext, signaling};
 	RemoteCommunication communication;
-
-	// Pending inbound request info (for respondToConnetion)
-	DiscoveryEndpoint	pendingRemote;
 
 	ConnectionState		connectionState{ConnectionState::None};
 };
@@ -46,10 +45,53 @@ netlink::NetLink::NetLink(NetLink &&) noexcept {}
 netlink::NetLink &netlink::NetLink::operator=(NetLink &&) noexcept = default;
 
 
-void			  netlink::NetLink::configure(const NetLinkConfig &config, const NetLinkCallbacks &callbacks) {}
+void			  netlink::NetLink::configure(const NetLinkConfig &config, const NetLinkCallbacks &callbacks)
+{
+	pImpl->config	 = config;
+	pImpl->callbacks = callbacks;
+	pImpl->connectionFlow.setLocalIP(config.localIPv4);
+
+	ConnectionFlowCallbacks flowCB;
+
+	// Remote wants to connect -> ask app to accept/decline
+	flowCB.onConnectionRequested = [this](const DiscoveryEndpoint &remote)
+	{
+		pImpl->connectionState = ConnectionState::None;
+		if (pImpl->callbacks.onConnectionChanged)
+			pImpl->callbacks.onConnectionChanged({ConnectionState::None, "", {remote.IPAddress, remote.port, remote.displayName}});
+	};
+
+	// TCP established -> init messaging
+	flowCB.onConnected = [this](ITCPSession::pointer session)
+	{
+		pImpl->communication.init(session, pImpl->config.secret);
+		pImpl->communication.start();
+		pImpl->connectionState = ConnectionState::Connected;
+
+		if (pImpl->callbacks.onConnectionChanged)
+			pImpl->callbacks.onConnectionChanged({ConnectionState::Connected, "", {}});
+	};
+
+	flowCB.onConnectionFailed = [this](const std::string &reason)
+	{
+		pImpl->connectionState = ConnectionState::Error;
+		if (pImpl->callbacks.onConnectionChanged)
+			pImpl->callbacks.onConnectionChanged({ConnectionState::Error, reason, {}});
+	};
+
+	flowCB.onDisconnected = [this]()
+	{
+		pImpl->communication.deinit();
+		pImpl->connectionState = ConnectionState::Disconnected;
+		if (pImpl->callbacks.onConnectionChanged)
+			pImpl->callbacks.onConnectionChanged({ConnectionState::Disconnected, "", {}});
+	};
+
+	pImpl->connectionFlow.setCallbacks(std::move(flowCB));
+}
 
 
-bool			  netlink::NetLink::init()
+bool netlink::NetLink::init()
 {
 	return false;
 }
@@ -88,39 +130,23 @@ std::vector<netlink::Endpoint> netlink::NetLink::getDiscoveredEndpoints()
 
 bool netlink::NetLink::connectTo(const Endpoint &remote)
 {
-	return false;
+	// Map public endpoint to internal DiscoveryEndpoint
+	DiscoveryEndpoint ep{remote.IPAddress, remote.port, remote.port, remote.displayName}; // @TODO: signalingport
+	pImpl->connectionFlow.requestConnection(ep);
+	
+	pImpl->connectionState = ConnectionState::Searching;
+
+	return true;
 }
 
 
 void netlink::NetLink::respondToConnection(bool accepted)
 {
-	auto &pending = pImpl->pendingRemote;
-
-	if (accepted)
-	{
-		pImpl->signaling.sendConnectAccept(pending.IPAddress, pending.signalingPort);
-
-		// TODO: Enter flow of establishing connection
-	}
-	else
-	{
-		pImpl->signaling.sendConnectDecline(pending.IPAddress, pending.signalingPort);
-		pImpl->connectionState = ConnectionState::None;
-	}
+	pImpl->connectionFlow.respondToConnection(accepted);
 }
 
 
-void netlink::NetLink::disconnect()
-{
-	// signal the remote peer before teardown
-	auto &pending = pImpl->pendingRemote;
-
-	if (pending.isValid())
-		pImpl->signaling.sendDisconnect(pending.IPAddress, pending.signalingPort);
-
-	pImpl->communication.deinit();
-	pImpl->connectionState = ConnectionState::Disconnected;
-}
+void					 netlink::NetLink::disconnect() {}
 
 
 netlink::ConnectionState netlink::NetLink::getConnectionState() const
