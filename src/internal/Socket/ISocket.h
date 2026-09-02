3bb14436e3c0a814dcae13a16a32efb8719e8eb7
Jens Langenberg <william@diversiam.com>
2026-09-02 21:32:42 +0200

    Added ISocket interface & helper structs in SocketTypes

---
+++ src/internal/Socket/ISocket.h
@@ -0,0 +1,31 @@
+/*
+==============================================================================
+	Module:         ISocket
+	Description:    Low-Level socket interface
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "SocketTypes.h"
+
+
+class ISocket
+{
+public:
+	virtual ~ISocket();
+
+	virtual NetLink::SocketBindResult bind(const std::string &address, const int port, const NetLink::BindOptions &options) = 0;
+
+	virtual int sendTo(const std::string &address, const int port, const void *data, int size) = 0;
+
+	virtual int receiveFrom(void *buffer, int maxSize, std::string &remoteHost, int &remotePort, int timeoutMS) = 0;
+
+	virtual bool waitUntilReady(bool forReading, int timeoutMS) = 0;
+
+	virtual void close() = 0;
+
+	virtual bool isOpen() = 0;
+
+	virtual int getBoundPort() const = 0;
+};
