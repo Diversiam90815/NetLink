5a717141381765c5bad8ac3df95457135976035b
Jens Langenberg <william@diversiam.com>
2026-09-06 15:20:21 +0200

    Finished impl of WinDataGramSocket

--- src/internal/Socket/ISocket.h
+++ src/internal/Socket/ISocket.h
@@ -15,17 +15,15 @@ class ISocket
 public:
 	virtual ~ISocket();
 
-	virtual NetLink::SocketBindResult bind(const std::string &address, const int port, const NetLink::BindOptions &options) = 0;
+	virtual NetLink::SocketBindResult bind(const std::string &address, const int port, const NetLink::BindOptions &options)		   = 0;
 
-	virtual int sendTo(const std::string &address, const int port, const void *data, int size) = 0;
+	virtual int						  sendTo(const std::string &address, const int port, const void *data, int size)			   = 0;
+	virtual int						  recvFrom(void *buffer, int maxSize, std::string &remoteHost, int &remotePort, int timeoutMS) = 0;
 
-	virtual int recvFrom(void *buffer, int maxSize, std::string &remoteHost, int &remotePort, int timeoutMS) = 0;
+	virtual bool					  waitUntilReady(bool forReading, int timeoutMS)											   = 0;
 
-	virtual bool waitUntilReady(bool forReading, int timeoutMS) = 0;
+	virtual void					  close()																					   = 0;
 
-	virtual void close() = 0;
-
-	virtual bool isOpen() const = 0;
-
-	virtual int getBoundPort() const = 0;
+	virtual bool					  isOpen() const																			   = 0;
+	virtual int						  getBoundPort() const																		   = 0;
 };
--- src/internal/Socket/WinDatagramSocket.cpp
+++ src/internal/Socket/WinDatagramSocket.cpp
@@ -7,12 +7,14 @@
 
 
 #include "WinDatagramSocket.h"
+#include "NetLinkLog.h"
+
 
 namespace
 {
 std::mutex gWinsSocketMutex;
-int        gWinsSocketRefCount = 0;
-}
+int		   gWinsSocketRefCount = 0;
+} // namespace
 
 
 WinsockScope::WinsockScope()
@@ -21,7 +23,7 @@ WinsockScope::WinsockScope()
 
 	if (gWinsSocketRefCount++ == 0)
 	{
-		WSADATA   wsaData{};
+		WSADATA	  wsaData{};
 		const int rc = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
 		if (rc != 0)
@@ -47,7 +49,7 @@ WinDatagramSocket::~WinDatagramSocket()
 NetLink::SocketBindResult WinDatagramSocket::bind(const std::string &address, const int port, const NetLink::BindOptions &options)
 {
 	NetLink::SocketBindResult result;
-	result.address       = address;
+	result.address		 = address;
 	result.requestedPort = port;
 
 	close();
@@ -64,7 +66,7 @@ NetLink::SocketBindResult WinDatagramSocket::bind(const std::string &address, co
 
 	sockaddr_in addr{};
 	addr.sin_family = AF_INET;
-	addr.sin_port   = htons(static_cast<u_short>(port));
+	addr.sin_port	= htons(static_cast<u_short>(port));
 
 	if (address.empty())
 		addr.sin_addr.s_addr = INADDR_ANY;
@@ -86,41 +88,117 @@ NetLink::SocketBindResult WinDatagramSocket::bind(const std::string &address, co
 
 	// retrieve actual port
 	sockaddr_in boundAddr{};
-	int         addrLen = sizeof(boundAddr);
+	int			addrLen = sizeof(boundAddr);
 	getsockname(mSocket, reinterpret_cast<sockaddr *>(&boundAddr), &addrLen);
-	mBoundPort = ntohs(boundAddr.sin_port);
+	mBoundPort		 = ntohs(boundAddr.sin_port);
 
-	mClosed          = false;
-	result.success   = true;
+	mClosed			 = false;
+	result.success	 = true;
 	result.boundPort = mBoundPort;
-	result.address   = address;
+	result.address	 = address;
 	return result;
 }
 
 
 int WinDatagramSocket::sendTo(const std::string &address, const int port, const void *data, int size)
 {
-	return 0;
+	if (mSocket == INVALID_SOCKET || mClosed.load())
+		return -1;
+
+	sockaddr_in destAddr{};
+	destAddr.sin_family = AF_INET;
+	destAddr.sin_port	= htons(static_cast<u_short>(port));
+	inet_pton(AF_INET, address.c_str(), &destAddr.sin_addr);
+
+	int result = sendto(mSocket, static_cast<const char *>(data), size, 0, reinterpret_cast<sockaddr *>(&destAddr), sizeof(destAddr));
+
+	if (result < 0)
+	{
+		int error = WSAGetLastError();
+
+		if (error != WSAEWOULDBLOCK)
+			NETLINK_LOG_ERROR("sendto failed: WSA error {} (host:{}, port:{}, size:{}", error, address, port, size);
+	}
+
+	return result;
 }
 
 
 int WinDatagramSocket::recvFrom(void *buffer, int maxSize, std::string &remoteAddress, int &remotePort, int timeoutMS)
 {
-	return 0;
+	if (mSocket == INVALID_SOCKET || mClosed.load())
+		return -1;
+
+	if (!waitUntilReady(true, timeoutMS))
+		return 0; // timeout
+
+	sockaddr_in fromAddr{};
+	int			fromLen = sizeof(fromAddr);
+
+	int			nBytes	= recvfrom(mSocket, static_cast<char *>(buffer), maxSize, 0, reinterpret_cast<sockaddr *>(&fromAddr), &fromLen);
+
+	if (nBytes > 0)
+	{
+		char ipStr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &fromAddr.sin_addr, ipStr, sizeof(ipStr));
+		remoteAddress = ipStr;
+		remotePort	  = ntohs(fromAddr.sin_port);
+	}
+
+	return nBytes;
 }
 
 
 bool WinDatagramSocket::waitUntilReady(bool forReading, int timeoutMS)
 {
-	return false;
+	if (mSocket == INVALID_SOCKET)
+		return false;
+
+	fd_set fdSet;
+	FD_ZERO(&fdSet);
+	FD_SET(mSocket, &fdSet);
+
+	timeval tv;
+	tv.tv_sec  = timeoutMS / 1000;
+	tv.tv_usec = (timeoutMS % 1000) * 1000;
+
+	int result = select(0, forReading ? &fdSet : nullptr, forReading ? nullptr : &fdSet, nullptr, &tv);
+	return result > 0;
 }
 
 
 void WinDatagramSocket::close()
 {
+	mClosed.store(true);
+
+	if (mSocket != INVALID_SOCKET)
+	{
+		::shutdown(mSocket, SD_BOTH);
+		closesocket(mSocket);
+		mSocket = INVALID_SOCKET;
+	}
+
+	mBoundPort = 0;
 }
 
 
 void WinDatagramSocket::applyOptions(const NetLink::BindOptions &options)
 {
+	auto setOpt = [this](int level, int name, int value, const char *label)
+	{
+		if (setsockopt(mSocket, level, name, reinterpret_cast<const char *>(&value), sizeof(value)) == SOCKET_ERROR)
+			NETLINK_LOG_WARNING("setsockopt({}) failed: WSA: {}", label, WSAGetLastError());
+	};
+
+	if (options.enableBroadcast)
+		setOpt(SOL_SOCKET, SO_BROADCAST, 1, "SO_BROADCAST");
+
+	if (options.receiveBufferSize > 0)
+		setOpt(SOL_SOCKET, SO_RCVBUF, options.receiveBufferSize, "SO_RCVBUF");
+
+	if (options.sendBufferSize > 0)
+		setsockopt(SOL_SOCKET, SO_SNDBUF, options.sendBufferSize, "SO_SNDBUF");
+
+	if (options.reuseAddress)
+		setOpt(SOL_SOCKET, SO_REUSEADDR, 1, "SO_REUSEADDR");
 }
