d920c374929def8214bf1db247632ded507abd4f
Jens Langenberg <william@diversiam.com>
2026-09-20 20:38:30 +0200

    Removed unused arguments that are logged if no logging is defined

--- src/internal/NetLinkLog.h
+++ src/internal/NetLinkLog.h
@@ -11,16 +11,22 @@
 
 #include "Logger.h"
 
-	#define NETLINK_LOG_DEBUG(...)	 LOG_DEBUG(__VA_ARGS__)
-	#define NETLINK_LOG_INFO(...)	 LOG_INFO(__VA_ARGS__)
-	#define NETLINK_LOG_WARNING(...) LOG_WARNING(__VA_ARGS__)
-	#define NETLINK_LOG_ERROR(...)	 LOG_ERROR(__VA_ARGS__)
+#define NETLINK_LOG_DEBUG(...)	 LOG_DEBUG(__VA_ARGS__)
+#define NETLINK_LOG_INFO(...)	 LOG_INFO(__VA_ARGS__)
+#define NETLINK_LOG_WARNING(...) LOG_WARNING(__VA_ARGS__)
+#define NETLINK_LOG_ERROR(...)	 LOG_ERROR(__VA_ARGS__)
 
 #else
 
-	#define NETLINK_LOG_DEBUG(...)	 ((void)0)
-	#define NETLINK_LOG_INFO(...)	 ((void)0)
-	#define NETLINK_LOG_WARNING(...) ((void)0)
-	#define NETLINK_LOG_ERROR(...)	 ((void)0)
+namespace netlink::internal
+{
+template <typename... Args>
+int logArgumentsUnused(const Args &...);
+} // namespace netlink::internal
+
+#define NETLINK_LOG_DEBUG(...)	 ((void)sizeof(::netlink::internal::logArgumentsUnused(__VA_ARGS__)))
+#define NETLINK_LOG_INFO(...)	 ((void)sizeof(::netlink::internal::logArgumentsUnused(__VA_ARGS__)))
+#define NETLINK_LOG_WARNING(...) ((void)sizeof(::netlink::internal::logArgumentsUnused(__VA_ARGS__)))
+#define NETLINK_LOG_ERROR(...)	 ((void)sizeof(::netlink::internal::logArgumentsUnused(__VA_ARGS__)))
 
 #endif
